761f82b96a72cb2603872f9e6814615d2b26ad84
hmcdowell95 <hmcdowell95@github.com>
2020-08-30 18:07:22 -0400

    render games destroy

--- pokemon_parties_api/app/controllers/games_controller.rb
+++ pokemon_parties_api/app/controllers/games_controller.rb
@@ -1,13 +1,16 @@
 class GamesController < ApplicationController
     def index
         games = Game.all 
-        render json: games, only: [:name, :trainer_name]
+        render json: games, only: [:id, :name, :trainer_name]
     end
 
     def create
         Game.create(name: params[:name], trainer_name: params[:trainer_name])
     end
 
-    
+    def destroy
+        game = Game.find(params[:id])
+        game.destroy
+    end
 
 end
