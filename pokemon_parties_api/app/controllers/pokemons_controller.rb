e51b8bd17ebbe4a0064fc8fc861cc15377e68bf7
hmcdowell95 <hmcdowell95@github.com>
2020-08-30 18:23:27 -0400

    render pokemon index

--- pokemon_parties_api/app/controllers/pokemons_controller.rb
+++ pokemon_parties_api/app/controllers/pokemons_controller.rb
@@ -1,2 +1,8 @@
 class PokemonsController < ApplicationController
+    def index
+        pokemons = Pokemon.all 
+        render json: pokemons, only: [:id, :nickname, :species, :type, :level, :game_id]
+    end
+
+    
 end
