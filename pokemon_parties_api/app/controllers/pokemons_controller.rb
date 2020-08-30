12ef1092ce0086006932652b0af1b2d7a0e0b18f
hmcdowell95 <hmcdowell95@github.com>
2020-08-30 18:35:44 -0400

    render pokemon create

--- pokemon_parties_api/app/controllers/pokemons_controller.rb
+++ pokemon_parties_api/app/controllers/pokemons_controller.rb
@@ -4,5 +4,9 @@ class PokemonsController < ApplicationController
         render json: pokemons, only: [:id, :nickname, :species, :type, :level, :game_id]
     end
 
+    def create
+        Pokemon.create(nickname: params[:nickname], species: params[:species], type: params[:type], level: params[:level], game_id: params[:game_id])
+    end
+
     
 end
