daed73ea4c922977fa0c11b9e28e934b743c0744
hmcdowell95 <hmcdowell95@github.com>
2020-08-30 17:19:43 -0400

    render games create

--- pokemon_parties_api/app/controllers/games_controller.rb
+++ pokemon_parties_api/app/controllers/games_controller.rb
@@ -3,5 +3,11 @@ class GamesController < ApplicationController
         games = Game.all 
         render json: games, only: [:name, :trainer_name]
     end
+
+    def create
+        Game.create(name: params[:name], trainer_name: params[:trainer_name])
+    end
+
     
+
 end
