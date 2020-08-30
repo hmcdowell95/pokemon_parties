b5d0385d2373b7a4fdfa3cf241e534e774354bcf
hmcdowell95 <hmcdowell95@github.com>
2020-08-30 16:00:23 -0400

    specify routes

--- pokemon_parties_api/config/routes.rb
+++ pokemon_parties_api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pokemons
-  resources :games
+  resources :pokemons, only: [:index, :create, :update, :destroy]
+  resources :games, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
