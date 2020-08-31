1b513cec81a7772e3e3e14c8a649a881e5277d67
hmcdowell95 <hmcdowell95@github.com>
2020-08-31 00:28:25 -0400

    js first function and base code

---
+++ pokemon_parties_frontend/index.js
@@ -0,0 +1,31 @@
+const BASE_URL = "http://localhost:3000"
+const GAME_URL = `${BASE_URL}/games`
+const POKEMON_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch(GAME_URL)
+    .then(resp => resp.json())
+    .then(json => rendergames(json))
+})
+
+function rendergames(info) {
+    for (const game of info) {
+        let a = document.createElement("div");
+        a.className = "card";
+        a.id = game.id;
+        a.innerHTML = `<h2>${game.name}</h2>
+        <button data-game-id="${game.id}">Add Pokemon</button>
+        <form id="${game.name}" class="hidden">
+        <input type="hidden" id="train" value="${game.id}">
+        <label>Pokemon species:</label>
+        <input type="text" id="new_species"><br>
+        <label>Nickname:</label>
+        <input type="text" id="new_name"><br>
+        <label>Level:</label>
+        <input type="text" id="levels"><br>
+        <input type="submit" id="submit-pokemon">
+        </form>
+        <ul></ul>`;
+        document.getElementsByTagName("body")[0].appendChild(a);
+    }
+}
