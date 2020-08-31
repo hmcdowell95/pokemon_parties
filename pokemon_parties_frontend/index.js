56681f8cb54dc2b12d8478984d639443bb3e2643
hmcdowell95 <hmcdowell95@github.com>
2020-08-31 13:22:22 -0400

    reconfigure functions

--- pokemon_parties_frontend/index.js
+++ pokemon_parties_frontend/index.js
@@ -10,23 +10,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function rendergames(info) {
     for (const game of info) {
-        let a = document.createElement("div");
-        a.className = "card";
-        a.id = game.id;
-        a.innerHTML = `<h2>${game.name}</h2>
-        <button data-game-id="${game.id}" class="add">Add Pokemon</button>
-        <form id="${game.name}" style="display: none">
-        <input type="hidden" id="train" value="${game.id}">
-        <label>Pokemon species:</label>
-        <input type="text" id="new_species"><br>
-        <label>Nickname:</label>
-        <input type="text" id="new_name"><br>
-        <label>Level:</label>
-        <input type="text" id="levels"><br>
-        <input type="submit" id="submit-pokemon">
-        </form>
-        <ul></ul>`;
-        document.getElementsByTagName("body")[0].appendChild(a);
+        new_game(game)
     }
 }
 
+function new_game(game) {
+    let a = document.createElement("div");
+    a.className = "card";
+    a.id = game.id;
+    a.innerHTML = `<h2>${game.name}</h2>
+    <button data-game-id="${game.id}" class="add">Add Pokemon</button>
+    <form id="${game.name}" style="display: none">
+    <input type="hidden" id="train" value="${game.id}">
+    <label>Pokemon species:</label>
+    <input type="text" id="new_species"><br>
+    <label>Nickname:</label>
+    <input type="text" id="new_name"><br>
+    <label>Level:</label>
+    <input type="text" id="levels"><br>
+    <input type="submit" id="submit-pokemon">
+    </form>
+    <ul></ul>`;
+    document.getElementsByTagName("body")[0].appendChild(a);
+}
